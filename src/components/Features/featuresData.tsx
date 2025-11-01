d082bd8316d2cb2dd4dc0a6e451f7cd2ec2a5049
AkshayaNikumbh <31890983+AkshayaNikumbh@users.noreply.github.com>
2025-11-01 16:07:23 +0530

    Update featuresData.tsx

--- src/components/Features/featuresData.tsx
+++ src/components/Features/featuresData.tsx
@@ -5,19 +5,19 @@ const featuresData: Feature[] = [
     id: 1,
     icon: "/images/brands/Princeton.svg",
     title: "Postdoctoral Research Associate",
-    paragraph: "Princeton University\nOctober 2021 - December 2023",
+    paragraph: "Princeton University\n 2021 - 2023",
   },
   {
     id: 2,
     icon: "/images/brands/UWV.png",
     title: "Fulbright Scholar",
-    paragraph: "University of Washington \n Sep 2019 - June 2020",
+    paragraph: "University of Washington \n 2019 - 2020",
   },
   {
     id: 3,
     icon: "/images/brands/IISc.jpg",
     title: "Ph.D.",
-    paragraph: "IISc Bangalore\nAug 2016 - Dec 2021",
+    paragraph: "IISc Bangalore\n 2016 - 2021",
   },
   {
     id: 4,
